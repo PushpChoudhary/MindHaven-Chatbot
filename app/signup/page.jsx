51d3c7559363678257be3268a7ee9f196ef6cc8b
CHETAN CHAUHAN <139692970+Chetan00000@users.noreply.github.com>
2025-09-09 16:48:52 +0530

    Update page.jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -18,7 +18,7 @@ export default function SignupPage() {
     setError('');
 
     try {
-      const res = await fetch('/app/api/signup', {
+      const res = await fetch('/app/api/signup/route.js', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
